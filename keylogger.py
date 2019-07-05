5ed3f852c8a014d7b311ceb24ae48c17a1939d92
mrtrue <gui15787@gmail.com>
2019-07-05 00:29:27 -0300

    nothing changed

--- keylogger.py
+++ keylogger.py
@@ -1,3 +1,5 @@
+#lib importantes
+#-*- coding: utf-8 -*-
 
 #Copyright (C) 2008 MrTrue <gui15787@gmail.com>
 #     This program is free software: you can redistribute it and/or modify
@@ -13,9 +15,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>
 
-
-#lib importantes
-#-*- coding: utf-8 -*-
 import pyxhook
 import datetime,os
 import smtplib
